c2ed3948bbd7e112bd8f76a3f3173800024c92d2
Ahmed-code-dotcom <-  >
2025-11-09 15:13:29 +0200

    nn

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://biqzcfbcsflriybyvtur.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpcXpjZmJjc2Zscml5Ynl2dHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczMDQsImV4cCI6MjA3NTMzMzMwNH0.J9kVaVrOpv83CQs6Q9N7TJQ34HGBbPR_1Vf_XaycMT0'; // زي ما عندك
+const supabaseUrl = 'https://biqzcfbcsflriybyvtur.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpcXpjZmJjc2Zscml5Ynl2dHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczMDQsImV4cCI6MjA3NTMzMzMwNH0.J9kVaVrOpv83CQs6Q9N7TJQ34HGBbPR_1Vf_XaycMT0'
 
-const access_token = localStorage.getItem('access_token');
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  global: {
-    headers: {
-      Authorization: access_token ? `Bearer ${access_token}` : '',
-    },
-  },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey)
