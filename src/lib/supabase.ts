4e8d9401ed2e25a85ae77a2b09be68854e3bc598
Ahmed-code-dotcom <-  >
2025-11-10 14:25:21 +0200

    zz

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://biqzcfbcsflriybyvtur.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpcXpjZmJjc2Zscml5Ynl2dHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk3NTczMDQsImV4cCI6MjA3NTMzMzMwNH0.J9kVaVrOpv83CQs6Q9N7TJQ34HGBbPR_1Vf_XaycMT0'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true, // يخلي السيشن محفوظة
+    autoRefreshToken: true, // يحدث التوكن تلقائيًا
+    detectSessionInUrl: true // ضروري لتسجيل الدخول من redirect
+  }
+})
